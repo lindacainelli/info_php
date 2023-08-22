6a44a348fb7e0afd9914937f096e40cc4c8e375a
lindacainelli <linlincainelli@gmail.comm>
2023-08-22 20:29:56 -0300

    futebol

--- futebol.php
+++ futebol.php
@@ -1,39 +1,4 @@
 <?php
-
-// Calcular os números primos 
-
-
-    $numero = 109; 
-    echo "Analisando o numero: $numero <br>";
-    $primos = 0; 
-    echo"Valores mútiplos: ";
-   
-
-    $a=0;
-    for ($i=1; $i<=$numero; $i++) {
- 
-      $resto = $numero % 2;
-    
-      if ($resto == 0) { 
-        break;
-      }
-      
-      $resto = $numero % $i;
-    
-      if ($resto != 0) { 
-        $a++;
-        echo "$i ";
-      }
-    }
-
-    if ($a >= 1) { 
-      echo"<br>Total de múltiplos: $a<br>";
-      echo"Resultado: $numero É primo!";
-    }else{ 
-      echo"<br>Total de múltiplos: $a<br>";
-      echo"Resultado: $numero Não é primo!";
-    }
-
 // 1 calcular o tempo de um jogo de futebol a partir das 
 // variaveis $horaInicial e $horaFinal e ao final exibir o tempo 
 // decorrido desse jogo. 
