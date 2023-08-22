bfcb2a82df2ecc2ea4a4f8177a0f9dba7e245d37
lindacainelli <linlincainelli@gmail.comm>
2023-08-22 19:42:30 -0300

    revisao

---
+++ primos.php
@@ -0,0 +1,37 @@
+<?php
+
+// Calcular os números primos 
+
+
+    $numero = 109; 
+    echo "Analisando o numero: $numero <br>";
+    $primos = 0; 
+    echo"Valores mútiplos: ";
+   
+
+    $a=0;
+    for ($i=1; $i<=$numero; $i++) {
+ 
+      $resto = $numero % 2;
+    
+      if ($resto == 0) { 
+        break;
+      }
+      
+      $resto = $numero % $i;
+    
+      if ($resto != 0) { 
+        $a++;
+        echo "$i ";
+      }
+    }
+
+    if ($a >= 1) { 
+      echo"<br>Total de múltiplos: $a<br>";
+      echo"Resultado: $numero É primo!";
+    }else{ 
+      echo"<br>Total de múltiplos: $a<br>";
+      echo"Resultado: $numero Não é primo!";
+    }
+
+
