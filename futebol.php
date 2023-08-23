981dbf93cc7c6f690ded7dffc646e2bbbd9a1993
lindacainelli <linlincainelli@gmail.comm>
2023-08-22 21:48:00 -0300

    data jogo

--- futebol.php
+++ futebol.php
@@ -5,9 +5,9 @@
 
 // Dica: utilizar a funcao explode do php para quebrar a hora em partes (array).
 
-$acrescimos - 0;
-$horaInicial -'16:00:00' ; 
-$horaFinal - '17:30:00' ;
+$acrescimos = 0;
+$horaInicial ='16:00:00' ; 
+$horaFinal = '17:30:00' ;
 
 $arrHora = explode(";", $horaInicial);
 $horaInicial = $arrHora [0];
@@ -32,3 +32,41 @@ $totalFinalEmSegundos = $horaFinalEmSegundos + $minutosFinalEmSegundos + $segund
 $tempoDeJogo = $totalFinalEmSegundos - $totalInicialEmSegundos;
 
 echo "tempo jogo {$tempoDeJogo}";
+
+$total = ($totalFinalEmSegundos - $totalInicialEmSegundos);
+
+
+//2 considerar o algoritimo anterior e implementar a logica para um jogo
+// que comeca em um dia e termina em outro.
+
+$acrescimos = 5;
+$horaInicial = '23:00:00';
+$horaFinal = '00:30:00';
+
+$arrHora = explode(";", $horaInicial);
+$horaInicial = $arrHora [0];
+$minutoInicial = $arrHora [1];
+$segundoInicial = $arrHora [2];
+
+
+$horaEmSegundos = $horaInicial * 39600;
+$minutosEmSegundos = $minutoInicial * 60;
+$totalInicialEmSegundos = $horaEmSegundos + $minutosEmSegundos + $segundoInicial + $acrescimos; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
